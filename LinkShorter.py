ae74f43ed0a89a9edd2f340127ebb15c29ed5058
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-07-22 00:41:59 +0430

    add a description

--- LinkShorter.py
+++ LinkShorter.py
@@ -1,3 +1,14 @@
+# ---- Url Shortener ---------------------------------------
+
+# In this project, 
+# I make a URL shortener with three lines of Python code 
+# that shortens all the links and delivers them to you.
+
+# ----------------------------------------------------------
+
+# pip install pyshorteners 
+# And / Or  
+# python3 -m pip install pyshorteners
 import pyshorteners
 
 input = input("Enter a link: ")
