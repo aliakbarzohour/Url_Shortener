f452926d043b96aa4946cd5f8454d3be3991c7db
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-07-22 00:45:50 +0430

    Clean Code .

--- LinkShorter.py
+++ LinkShorter.py
@@ -1,15 +1,16 @@
 # ---- Url Shortener ---------------------------------------
 
-# In this project, 
-# I make a URL shortener with three lines of Python code 
+# In this project,
+# I make a URL shortener with three lines of Python code
 # that shortens all the links and delivers them to you.
 
 # ----------------------------------------------------------
 
-# pip install pyshorteners 
-# And / Or  
+# pip install pyshorteners
+# And / Or
 # python3 -m pip install pyshorteners
 import pyshorteners
-
+# Get the address of the link
 input = input("Enter a link: ")
+# Show the Short Version of the link
 print("Shortened link: " + pyshorteners.Shortener().tinyurl.short(input))
